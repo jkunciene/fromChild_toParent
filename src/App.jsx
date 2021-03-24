86b0efd886345afe3ddea21a1e8daff124bc119f
Jolita <j.kunciene@gmail.com>
2021-03-24 14:48:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GrandParent from './GrandParent';
+
+function App() {
+  return (
+    <main>   
+      <GrandParent/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Child.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Child = params => {
+  return (
+    <>
+      <button onClick={() => params.setName("i'm from Child")}>
+        from Child
+      </button>
+    </>
+  );
+};
+
+export default Child;
